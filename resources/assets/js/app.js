ad4ad595da06b87b35e7a0a63c113bd2e8c9c581
Marc <mcalafellsmx@gmail.com>
2016-10-14 16:06:22 +0200

    prototypes

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -18,3 +18,21 @@ Vue.component('example', require('./components/Example.vue'));
 const app = new Vue({
     el: '#app'
 });
+
+var Animal= function () {
+    if(this.constructor === Animal){
+        throw new Error("AbstractClass");
+
+    }
+}
+
+
+Animal.prototype.say=function() {
+    throw new Error("AbstractMethod");
+
+}
+
+Animal.prototype.eat=function() {
+    console.log("animal eating");
+
+}
