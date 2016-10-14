9d198a5dce0d5c9fb1dc4ad740591a3e1ffa64d3
Marc <mcalafellsmx@gmail.com>
2016-10-14 16:25:49 +0200

    override cat say

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -42,4 +42,9 @@ var Cat =function () {
     Animal.apply(this, arguments);
 }
 
+Cat.prototype = Object.create(Animal.prototype);
+Cat.prototype.constructor = Cat;
 
+Cat.prototype.say= function(){
+    console.log("miauu");
+}
